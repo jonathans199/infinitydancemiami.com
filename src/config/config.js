bd54ec1c0a1c1b603764d13a1ee147cd1675456e
andre <defauult@default.com>
2018-08-13 12:25:47 -0400

    config change

--- src/config/config.js
+++ src/config/config.js
@@ -1,9 +1,9 @@
 
 // This is where the front end connects to make api calls
-const defaultURL      = "http://localhost:8086"
+// const defaultURL      = "http://localhost:8086"
 // This is the email where the forms will be delivered
 const deliveryEmail   = 'mdiaz00147@gmail.com'
-// const defaultURL    = "http://infinitydancemiami.com"
+const defaultURL    = ""
 
 module.exports = {
   defaultURL: defaultURL,
