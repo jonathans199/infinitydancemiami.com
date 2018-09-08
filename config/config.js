7b7205f377d2f6939552b58efb9373ecbd9a5ebc
Jonathan Sanchez <josano@Jonathans-MacBook-Pro.local>
2018-09-08 13:31:56 -0400

    changes on server

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 'use strict';
-const apiPort   = 3002 // This is the port where node API runs
+const apiPort   = 8086 // This is the port where node API runs
 const defaultURL = "http://localhost:" + apiPort // This is the api endpoint
       // defaultURL   = "" // if modified here it means it is for production mode
 
