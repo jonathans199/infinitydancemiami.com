db68547746a4a5c9533207c2d50eb65aea651a51
Jonathan Sanchez <josano@Jonathans-MacBook-Pro.local>
2018-09-09 22:46:22 -0400

    pulling from master

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 'use strict';
 const apiPort   = 8086 // This is the port where node API runs
-const defaultURL = "http://localhost:" + apiPort // This is the api endpoint
-      // defaultURL   = "" // if modified here it means it is for production mode
+let defaultURL = "http://localhost:" + apiPort // This is the api endpoint
+    defaultURL   = "" // if modified here it means it is for production mode
 
 const deliveryEmail = "jonathan@asoftio.com // This is the email where forms messages will be delivered"
 
