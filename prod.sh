dfad1f83e55fb7baad9f28a1162782a959adae34
jsanchez@legacyresearch.com <jsanchez@legacyresearch.com>
2020-08-19 11:58:32 -0400

    updated prod.sh and pricing

--- prod.sh
+++ prod.sh
@@ -1,3 +1,3 @@
 rm -rf html/
 npm run build -p
-rsync -azP html/* root@185.39.11.13:/usr/share/nginx/asoftio/clients/infinitydancemiami/prod/infinitydancemiami/
+rsync -azP -e 'ssh -p 2222' html/* root@185.39.11.13:/usr/share/nginx/asoftio/clients/infinitydancemiami/prod/infinitydancemiami/
